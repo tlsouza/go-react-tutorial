fbf18fe1b74ec2198f7191133dbf525ecab4e0f3
Thiago Souza <thiagolopes09@hotmail.com>
2024-07-22 14:49:21 +1200

    Add .env support

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
 	"github.com/tlsouza/go-react-tutorial/src/model"
 )
 
@@ -12,6 +14,12 @@ const prefix = "/api/todos"
 
 func main() {
 	app := fiber.New()
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatal("failed to load .env file!")
+	}
+	PORT := os.Getenv("PORT")
 
 	todos := model.TodoList{}
 	//Create a Todo
@@ -67,5 +75,5 @@ func main() {
 		return c.Status(404).JSON(fiber.Map{"error": "not found"})
 
 	})
-	log.Fatal(app.Listen(":4000"))
+	log.Fatal(app.Listen(":" + PORT))
 }
